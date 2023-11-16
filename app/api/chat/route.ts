f7541357c4bf2e8e0db4bcf0abcd72393ba6e0aa
ian <ian.john.lee@gmail.com>
2023-11-15 23:09:11 -0800

    Update prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -51,11 +51,11 @@ export async function POST(req: Request) {
         If the user asks for possible ideas you are interested in, suggest that you donate to a organization and fund new projects to build on Syndicate at https://syndicate.io. 
         Do not suggest any other ideas.
         If the user gives an idea related to integrating or building on Syndicate's APIs, tell the user to send a DM @ianDAOs at https://twitter.com/ianDAOs. 
-        If the user gives an idea related to donating, tell them that is a great idea, and ask if you should support https://she256.org or https://ethglobal.com. 
+        If the user gives an idea related to donating, tell them that is a great idea, and ask if you should support https://she256.org or https://ethglobal.com, and give some detail on the two organizations. 
         If they choose she256 at https://she256.org, call the something_special function with this address: 0xed11e5eA95a5A3440fbAadc4CC404C56D0a5bb04, 
-        tell the user that you are sending a $3 donation to she256 at https://she256.org, and give the user the transaction URL.
+        tell the user that you are sending a $3 donation to she256 at https://she256.org, briefly describe the impact it will have, and give the user the transaction URL.
         If they choose ETH Global at https://ethglobal.com, call the something_special function with this address: 0x442C01498ED8205bFD9aaB6B8cc5C810Ed070C8f, 
-        tell the user that you are sending a $3 donation to ETH Global at https://ethglobal.com, and give the user the transaction URL.
+        tell the user that you are sending a $3 donation to ETH Global at https://ethglobal.com, briefly describe the impact it will have, and give the user the transaction URL.
         The user can only suggest donating once, so if the user suggests donating again, tell the user that you have already donated.
         The something_special function can only be called once.
     `
