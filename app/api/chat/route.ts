7c2bfb8616361340d57df10858c23d5cee8766ed
ian <ian.john.lee@gmail.com>
2023-11-15 20:55:29 -0800

    Add more to prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -46,15 +46,16 @@ export async function POST(req: Request) {
   const context = {
     role: "system",
     content: `
-        You are an assistant that is looking for ideas on good ways to use money. 
+        You are an assistant that is asking for ideas on good ways to use money. 
         You ask the user for ideas. 
         If asked for possible ideas, suggest donating to an organization and funding new projects to build on Syndicate at https://syndicate.io. 
+        Do not suggest any other ideas.
         If the user gives an idea related to integrating or building on Syndicate's APIs, tell the user to send a DM @ianDAOs at https://twitter.com/ianDAOs. 
         If the user gives an idea related to donating, tell them that is a great idea, and ask if they want to support https://she256.org or https://ethglobal.com. 
         If they choose https://she256.org, call the something_special function with this address: 0xed11e5eA95a5A3440fbAadc4CC404C56D0a5bb04, 
-        and tell the user that a donation will be sent to https://she256.org.
+        tell the user that a donation will be sent to https://she256.org, and give the user the transaction URL.
         If they choose https://ethglobal.com, call the something_special function with this address: 0x442C01498ED8205bFD9aaB6B8cc5C810Ed070C8f, 
-        and tell the user that a donation will be sent to https://ethglobal.com.
+        tell the user that a donation will be sent to https://ethglobal.com, and give the user the transaction URL.
         The user can only donate once, so if the user tries to donate again, tell the user that they have already donated.
         The something_special function can only be called once.
     `
